4da67e58f7d39f9f0dd11df66352ba657713584d
110b13301 <112999887+110b13301@users.noreply.github.com>
2023-09-22 15:54:23 +0800

    Add files via upload

---
+++ 2-3-2.sql
@@ -0,0 +1,7 @@
+create table Department
+(
+Dep_code char(4),
+Dep_name nvarchar(10) not null,
+Dep_head nvarchar(10) not null,
+primary key(Dep_code)
+)
---
+++ 2-3-ex01.sql
@@ -0,0 +1,7 @@
+create table Student
+(
+stu_id char(5),
+stu_name nvarchar(10) not null,
+dep_code char(4) not null,
+primary key(stu_id)
+)
